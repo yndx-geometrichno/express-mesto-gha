03db4dc58141c9d58a048c25e6da72e709936ef3
yndx_geometrichno <klementevart@yandex.ru>
2023-11-14 19:21:24 +0500

    refactror: upd card and user models

--- models/card.js
+++ models/card.js
@@ -1,31 +1,39 @@
 const { Schema, model } = require("mongoose");
 
-const cardSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    minlegth: 2,
-    maxlength: 30,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  likes: [
-    {
-      type: Schema.Types.ObjectId,
-      default: [],
+const cardSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: {
+        value: true,
+        message: "Поле имя является обязательным",
+      },
+      minlegth: [2, "Минимальная длина 2 символа"],
+      maxlength: [30, "Максимальная длина 30 символов"],
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    link: {
+      type: String,
+      required: {
+        value: true,
+        message: "Поле ссылка является обязательным",
+      },
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        default: [],
+      },
+    ],
   },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 module.exports = model("card", cardSchema);
--- models/user.js
+++ models/user.js
@@ -1,19 +1,30 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    minlegth: 2,
-    maxlength: 30,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: {
+        value: true,
+        message: "Поле имя является обязательным",
+      },
+      minlegth: [2, "Минимальная длина 2 символа"],
+      maxlength: [30, "Максимальная длина 30 символов"],
+    },
+    about: {
+      type: String,
+      required: {
+        value: true,
+        message: "Поле о себе является обязательным",
+      },
+      minlegth: [2, "Минимальная длина 2 символа"],
+      maxlength: [30, "Максимальная длина 30 символов"],
+    },
+    avatar: String,
   },
-  about: {
-    type: String,
-    required: true,
-    minlegth: 2,
-    maxlength: 30,
-  },
-  avatar: String,
-});
+  {
+    versionKey: false,
+  }
+);
 
-module.exports = model('user', userSchema);
+module.exports = model("user", userSchema);
