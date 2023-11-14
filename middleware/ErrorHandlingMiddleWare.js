88fde2028ba91a4d582e1cde3a02c58bf6046c1c
yndx_geometrichno <klementevart@yandex.ru>
2023-11-14 19:42:43 +0500

    fix

--- middleware/ErrorHandlingMiddleWare.js
+++ middleware/ErrorHandlingMiddleWare.js
@@ -1,5 +1,6 @@
 const ApiError = require("../error/ApiError");
 
+// eslint-disable-next-line no-unused-vars
 module.exports = function (err, req, res, next) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message });
