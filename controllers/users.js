dc358be29b7cde06f5f53cf0e7e36ed608d95fc8
yndx_geometrichno <klementevart@yandex.ru>
2023-11-14 19:19:55 +0500

    refactor: user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,82 @@
 const User = require("../models/user");
+const ApiError = require("../error/ApiError");
 
-module.exports.getUsers = (req, res) => {
-  User.find({})
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "проищошла ошибка", err }));
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    return res.send(users);
+  } catch (err) {
+    return next();
+  }
 };
 
-module.exports.getUser = (req, res) => {
-  User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка", err }));
+const getUser = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("NotFound");
+    }
+    return res.send(user);
+  } catch (err) {
+    if (err.message === "NotFound") {
+      return next(ApiError.badRequest("Пользователь по указанному _id не найден"));
+    }
+    if (err.name === "CastError") {
+      return next(ApiError.invalid("Id is not valid"));
+    }
+    return next(err);
+  }
 };
 
-module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "произошла ошибка", err }));
+const createUser = async (req, res, next) => {
+  try {
+    const { name, about, avatar } = req.body;
+    const newUser = await User.create({ name, about, avatar });
+    return res.send(await newUser.save());
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return next(ApiError.invalid("Переданы некорректные данные при создании пользователя"));
+    }
+    return next(err);
+  }
 };
 
-module.exports.updateUserInfo = (req, res) => {
-  const { name, about } = req.body;
-
-  User.findOneAndUpdate(req.body._id, { $set: { name, about } }, { new: true })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "произошла ошибка", err }));
+const updateUserInfo = async (req, res, next) => {
+  try {
+    const { name, about } = req.body;
+    const updateUser = User.findOneAndUpdate(
+      req.body._id,
+      { $set: { name, about } },
+      { new: true }
+    );
+    return res.send(updateUser);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return next(ApiError.invalid("Переданы некорректные данные при обновлении профиля"));
+    }
+    return next(err);
+  }
 };
 
-module.exports.updateUserAvatar = (req, res) => {
-  const { avatar } = req.body;
+const updateUserAvatar = (req, res, next) => {
+  try {
+    const { avatar } = req.body;
+    const userUpdateAvatar = User.findOneAndUpdate(req.body._id, { $set: { avatar } }, { new: true });
+    return res.send(userUpdateAvatar);
 
-  User.findOneAndUpdate(req.body._id, { $set: { avatar } }, { new: true })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "произошла ошибка", err }));
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      return next(ApiError.invalid("Переданы некорректные данные при обновлении аватара."));
+    }
+    next(err)
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUserInfo,
+  updateUserAvatar,
 };
