2aca2998fa5811cda6ecd273e3ecce6181925a2a
yndx_geometrichno <klementevart@yandex.ru>
2023-11-15 11:35:34 +0500

    feat: add helmet extension + some code refactor

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleWare");
 
-const PORT = /*process.env.PORT ||*/ 3000;
+const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
+  process.env;
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {});
+mongoose.connect(DB_URL, {});
 
 app.use((req, res, next) => {
   req.user = {
@@ -20,10 +23,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use("/", router);
-
 app.use(errorHandler);
-app.use("*", (req, res) => {
-  res.status(404).send({ message: "This page is not exist" });
-});
 
-app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
+app.listen(PORT);
