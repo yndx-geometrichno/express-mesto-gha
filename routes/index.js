34dbb9414a37488942c464a86035c1647a62d1af
yndx_geometrichno <klementevart@yandex.ru>
2023-11-24 19:09:16 +0500

    feat: add login, register routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,39 @@
 // const router = require("express").Router();
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 const userRouter = require("./users");
 const cardRouter = require("./cards");
+const auth = require("../middleware/auth");
+const { createUser, login } = require("../controllers/users");
 
+router.post(
+  "/signup",
+  celebrate({
+    body: Joi.object()
+      .keys({
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+        name: Joi.string().default("Жак-Ив Кусто"),
+        about: Joi.string().default("Исследователь"),
+        avatar: Joi.string().default(
+          "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png"
+        ),
+      })
+      .unknown(true),
+  }),
+  createUser
+);
+router.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+router.use(auth);
 router.use("/users", userRouter);
 router.use("/cards", cardRouter);
 router.use("*", (req, res) => {
