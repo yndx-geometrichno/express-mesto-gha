ea074152cf6fe83bd1633b12bfd95761aafda584
yndx_geometrichno <klementevart@yandex.ru>
2023-11-26 13:54:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const { errors } = require("celebrate");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleWare");
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
-  process.env;
+const { DB_PORT, DB_URL } = process.env;
 
 const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(DB_URL, {});
 
@@ -20,4 +22,4 @@ app.use("/", router);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT);
+app.listen(DB_PORT);
