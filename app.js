a2ffb221112ac936fe649a0a8cd0b45592e5cef5
yndx_geometrichno <klementevart@yandex.ru>
2023-11-26 14:08:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(DB_URL, {});
+mongoose.connect(String(DB_URL), {});
 
 app.use("/", router);
 app.use(errors());
