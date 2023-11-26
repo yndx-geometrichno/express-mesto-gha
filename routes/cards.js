0760431e42b52ec94a875d65ab03d931e58f976f
yndx_geometrichno <klementevart@yandex.ru>
2023-11-26 16:51:17 +0500

    fix: Joi validation bugfix

--- routes/cards.js
+++ routes/cards.js
@@ -9,6 +9,9 @@ const {
   dislikeCard,
 } = require("../controllers/cards");
 
+const URL_REGEX =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
 router.get("/", getCards);
 router.get("/:cardId", getCard);
 router.post(
@@ -17,7 +20,7 @@ router.post(
     body: Joi.object()
       .keys({
         name: Joi.string().required().min(2).max(30),
-        link: Joi.string().uri().required(),
+        link: Joi.string().regex(URL_REGEX).required(),
       })
       .unknown(true),
   }),
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 const {
   getUsers,
   getUser,
@@ -6,11 +7,41 @@ const {
   updateUserAvatar,
   updateUserInfo,
 } = require("../controllers/users");
+const validateId = require("../middleware/validateId");
+
+const URL_REGEX =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 router.get("/", getUsers);
 router.get("/me", getMe);
-router.get("/:userId", getUser);
-router.patch("/me", updateUserInfo);
-router.patch("/me/avatar", updateUserAvatar);
+router.get(
+  "/:userId",
+  celebrate({
+    params: {
+      userId: Joi.required(),
+    },
+  }),
+  validateId,
+  getUser
+);
+router.patch(
+  "/me",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUserInfo
+);
+router.patch(
+  "/me/avatar",
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().regex(URL_REGEX),
+    }),
+  }),
+  updateUserAvatar
+);
 
 module.exports = router;
