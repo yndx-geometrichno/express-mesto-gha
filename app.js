fceaf9bf1f2de1f19f5321601d03c2ecfe24562a
yndx_geometrichno <klementevart@yandex.ru>
2023-11-27 00:24:42 +0500

    feat: add express-rate-limit to protect from DoS-attacks

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const { rateLimit } = require("express-rate-limit");
 const cors = require("cors");
 const helmet = require("helmet");
 const { errors } = require("celebrate");
@@ -11,10 +12,17 @@ const errorHandler = require("./middleware/ErrorHandlingMiddleWare");
 // const { DB_PORT, DB_URL } = process.env;
 const DB_PORT = 3000;
 const DB_URL = "mongodb://127.0.0.1:27017/mestodb";
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: 'Too many requests from this IP, please try again later.'
+})
+
 
 const app = express();
 app.use(helmet());
 app.use(cors());
+app.use(limiter)
 app.use(express.json());
 app.use(cookieParser());
 
