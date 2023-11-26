485a327e2340876fed4fbe82fd34d02a190ae091
yndx_geometrichno <klementevart@yandex.ru>
2023-11-26 13:53:40 +0500

    feat: url validation by regex

--- models/user.js
+++ models/user.js
@@ -35,7 +35,7 @@ const userSchema = new Schema(
       default:
         "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
       validate: {
-        validator (v) {
+        validator(v) {
           return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(
             v
           );
